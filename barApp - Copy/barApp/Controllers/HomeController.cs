443202ad38aa69c8b1be023d00ff9e7c78d89a4a
Dexter <54725203+DXsyntax@users.noreply.github.com>
2021-07-21 01:03:27 +0800

    commit

---
+++ barApp - Copy/barApp/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace barApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Dine()
+        {
+
+            return View();
+        }
+
+        public ActionResult Pickup()
+        {
+            return View();
+
+        }
+
+        public ActionResult Delivery()
+        {
+            return View();
+
+        }
+
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+    }
+}

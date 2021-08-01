97ca071aad944c50982337c5766c088bb6c764e0
Dexter <54725203+DXsyntax@users.noreply.github.com>
2021-08-01 08:48:20 +0800

    upload latest barapp

---
+++ barApp_latest_1aug/barApp/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // added in
+using System.Linq;
+using System.Web;
+
+namespace barApp.Models
+{
+    [Table("ACCOUNT")]
+    public class Account
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
